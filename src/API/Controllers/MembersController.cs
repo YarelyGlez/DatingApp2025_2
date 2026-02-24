937a4ae98afeba27f48c36cd7e63ab4a1192817c
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2026-02-24 14:16:48 -0600

    Parcial04 - Add member update persistance

--- src/API/Controllers/MembersController.cs
+++ src/API/Controllers/MembersController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using API.Mappers;
@@ -29,5 +31,36 @@ public class MembersController(IMembersRepository membersRepository) : BaseApiCo
     public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhotos(string id)
     {
         return Ok(await membersRepository.GetPhotosAsync(id));
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(MemberUpdateRequest request)
+    {
+        var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (memberId == null)
+        {
+            return BadRequest("No id found in token");
+        }
+
+        var member = await membersRepository.GetMemberAsync(memberId);
+        if (member == null)
+        {
+            return BadRequest("Failed to get member"); 
+        }
+
+        member.DisplayName = request.DisplayName ?? member.DisplayName;
+        member.Description = request.Description ?? member.Description;
+        member.City = request.City ?? member.City;
+        member.Country = request.Country ?? member.Country;
+
+        membersRepository.Update(member);
+
+        if (await membersRepository.SaveAllAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Failed to update profile");
     }
 }
