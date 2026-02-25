b20207b3751af4428dde910509061cadb36b707a
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2026-02-25 13:36:51 -0600

    Parcial04 - Add photo upload

--- src/API/Data/MembersRepository.cs
+++ src/API/Data/MembersRepository.cs
@@ -13,7 +13,10 @@ public class MembersRepository(AppDbContext context) : IMembersRepository
 
     public async Task<Member?> GetMemberForUpdateAsync(string id)
     {
-        return await context.Members.Include(m => m.User).SingleOrDefaultAsync(m => m.Id == id);
+        return await context.Members
+                            .Include(m => m.User)
+                            .Include(m => m.Photos)
+                            .SingleOrDefaultAsync(m => m.Id == id);
     }
 
     public async Task<IReadOnlyList<Member>> GetMembersAsync()
