92e0406bbb4260b3572be8c9f80c2651aee892d8
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2026-03-27 12:53:36 -0600

    Parical05 - Fix modal and restore cache

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -63,6 +63,7 @@ export class MemberList implements OnInit {
 
   resetFilters() {
     this.memberParams = new MemberParams();
+    this.updatedParams = new MemberParams();
     this.loadMembers();
   }
 
