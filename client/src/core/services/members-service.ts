bc4609ed8dcd36f2ac90f2a23fc4a2010f7ef81a
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2026-03-27 12:36:57 -0600

    Parical05 - Add filters to local storage

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -32,7 +32,11 @@ export class MembersService {
 
     if(memberParams.gender) params = params.append('gender', memberParams.gender);
 
-    return this.http.get<PaginationResult<Member>>(this.baseUrl + "members", { params });
+    return this.http.get<PaginationResult<Member>>(this.baseUrl + "members", { params }).pipe(
+      tap(() => {
+        localStorage.setItem('filters', JSON.stringify(memberParams));
+      })
+    );
   }
 
   getPhotos(id: string) {
