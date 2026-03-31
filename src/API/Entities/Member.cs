83bd633914e65e9ab9abcee8677f877cdd47a6a9
YarelyGlez <112435684+YarelyGlez@users.noreply.github.com>
2026-03-31 12:11:25 -0600

    Parcial05 - Add likes entity

--- src/API/Entities/Member.cs
+++ src/API/Entities/Member.cs
@@ -24,4 +24,10 @@ public class Member
     [JsonIgnore]
     public List<Photo> Photos { get; set; } = [];
 
+    [JsonIgnore]
+    public List<MemberLike> LikedByMembers { get; set; } = [];
+    
+    [JsonIgnore]
+    public List<MemberLike> LikedMembers { get; set; } = [];
+
 }
